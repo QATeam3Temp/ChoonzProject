5c3d974611e68f944b53bdaa5f23b53d1fe5cf15
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-04-06 12:50:51 +0100

    added the correct annotations for the domains

--- src/main/java/com/qa/choonz/persistence/domain/Playlist.java
+++ src/main/java/com/qa/choonz/persistence/domain/Playlist.java
@@ -6,16 +6,19 @@ import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.qa.choonz.rest.dto.PlaylistDTO;
 
 @Entity
+@Table(name = "playlist")
 public class Playlist {
 
     @Id
@@ -37,7 +40,7 @@ public class Playlist {
     @Column(unique = true)
     private String artwork;
 
-    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Track> tracks;
 
     public Playlist() {
