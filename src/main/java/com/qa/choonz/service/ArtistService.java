c96d9e315aa6defc6e91c09566a78ec6f5d46e55
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-04-06 09:46:58 +0100

    MW-18 #comment changed update artist controller and service so that a DTO is accpeted

--- src/main/java/com/qa/choonz/service/ArtistService.java
+++ src/main/java/com/qa/choonz/service/ArtistService.java
@@ -46,7 +46,7 @@ public class ArtistService {
         return this.mapToDTO(newFound);
     }
 
-    public ArtistDTO update(Artist artist, long id) {
+    public ArtistDTO update(ArtistDTO artist, long id) {
         Artist toUpdate = this.repo.findById(id).orElseThrow(ArtistNotFoundException::new);
         toUpdate.setName(artist.getName());
         toUpdate.setAlbums(artist.getAlbums());
