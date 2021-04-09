97a74e764793ab051a0c4d013cc3e66874310214
Nik-Pap <nikospap8@hotmail.com>
2021-04-09 10:19:46 +0100

    changed made in home page

--- src/main/resources/static/js/front.js
+++ src/main/resources/static/js/front.js
@@ -5,3 +5,33 @@ $(function() {
   });
 });
 
+
+function setUser() {
+  let user1 = document.querySelector("#navbar-right");
+  if (user1 != null) {
+    user1.innerHTML=""
+
+  }
+}
+
+
+
+
+function load() {
+  let logout = document.createElement("a");
+let x = document.querySelector('a');
+let div = document.querySelector("#navbar-right");
+logout.innerHTML="Replaced"
+div.innerHTML=""
+
+  if (document.cookie) {
+    showloggedin() 
+  } else {
+    showguest()
+  }
+}
+
+
+function showloggedin() {
+
+}
