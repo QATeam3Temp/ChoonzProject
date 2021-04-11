7ca1e631392ac2445fb63a0209a4528cb93f6ac6
Nik-Pap <nikospap8@hotmail.com>
2021-04-11 18:33:02 +0100

    created mock pages for frontend

--- src/main/resources/static/js/front.js
+++ src/main/resources/static/js/front.js
@@ -18,30 +18,3 @@ window.addEventListener('scroll', function() {
 });
 
 
-// function setUser() {
-//   let user1 = document.querySelector("#navbar-right");
-//   if (user1 != null) {
-//     user1.innerHTML=""
-
-//   }
-// }
-
-
-// function load() {
-//   let logout = document.createElement("a");
-// let x = document.querySelector('a');
-// let div = document.querySelector("#navbar-right");
-// logout.innerHTML="Replaced"
-// div.innerHTML=""
-
-//   if (document.cookie) {
-//     showloggedin() 
-//   } else {
-//     showguest()
-//   }
-// }
-
-
-// function showloggedin() {
-
-// }
