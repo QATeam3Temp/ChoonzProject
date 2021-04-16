5f066b54812f3117b752d398102bc280c93a283b
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-04-16 08:59:32 +0100

    added sort button for featured artists

--- src/main/resources/static/js/artist.js
+++ src/main/resources/static/js/artist.js
@@ -6,8 +6,9 @@ function getUrlVars() {
   return vars;
 }
 
+const artistId = getUrlVars()['id'];
+
 async function fetchArtist() {
-  const artistId = getUrlVars()['id'];
   const response = await fetch(`http://localhost:8082/artists/read/id/${artistId}`);
 
   if (!response.ok) {
@@ -29,17 +30,31 @@ async function fetchAlbums() {
   return albums;
 }
 
+function sort() {
+  const albumCards = document.querySelectorAll('.col-lg-4.col-sm-6.mb-4');
+  const sortButton = document.querySelector('#sort');
+  for (let i = 0; i < albumCards.length; i++) {
+    if (albumCards[i].getAttribute('data-featuredartists') < 1) {
+      if (albumCards[i].style.display == 'block') {
+        albumCards[i].style.display = 'none';
+        sortButton.innerHTML = 'Click to view all';
+      } else {
+        albumCards[i].style.display = 'block';
+        sortButton.innerHTML = 'Click for albums with featured Artists';
+      }
+    }
+  }
+}
+
 fetchAlbums()
   .then((albums) => {
     fetchArtist()
       .then((artist) => {
-        console.log(albums);
         const artistName = document.querySelector('.my-4');
         const albumRow = document.querySelector('.row');
 
         artistName.innerHTML = artist.name;
         document.title = `Choonz Tracks | ${artist.name}`;
-        console.log(artist);
 
         for (const album of artist.albums) {
           const cardContainer = document.createElement('div');
@@ -50,11 +65,16 @@ fetchAlbums()
 
           for (let i = 0; i < albums.length; i++) {
             if (albums[i].id === album) {
+              cardContainer.id = albums[i].id;
+              cardContainer.setAttribute('style', 'display: block');
+              if (albums[i].featuredArtists == 0) {
+                cardContainer.setAttribute('data-featuredArtists', '0');
+              }
+              cardContainer.setAttribute('data-featuredArtists', albums[i].featuredArtists);
               cardContainer.className = 'col-lg-4 col-sm-6 mb-4';
               cardWrapper.className = 'card h-100';
               cardBody.className = 'card-body';
               albumTitle.className = 'card-title';
-
               albumTitle.innerHTML = albums[i].name;
               albumLink.innerHTML = 'click here to view';
               albumLink.href = '/album?id=' + album;
